e30bdc5664638c213026b3adafafe47b7fe20534
Rajendra More <158751784+rajendra0303@users.noreply.github.com>
2024-07-14 15:34:31 +0530

    new class added

---
+++ Opps/Palindrom.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps
+{
+    internal class Palindrom
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a Number");
+            int num = int.Parse(Console.ReadLine());
+
+            int temp = num, rev = 0;
+
+            while(temp > 0)
+            {
+                int rem = temp % 10;
+                rev = rev * 10 + rem;
+                temp /= 10;
+            }
+            if(rev== num)
+            {
+                Console.WriteLine("No is Palindrom");
+
+            }
+            else
+            {
+                Console.WriteLine("No is not Palindrom");
+            }
+        }
+
+       
+    }
+   
+}
