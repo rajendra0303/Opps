dfbbe04475595b8c24717e64eac6f9d13fd61f2f
Rajendra More <158751784+rajendra0303@users.noreply.github.com>
2024-07-17 16:30:10 +0530

    Dyanamic Polymorphism

---
+++ Opps/CompileTime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps
+{
+    class MathodOverLoading
+    {
+        public void Show(String a, String b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void Show(String a, String b, string c)
+        {
+
+
+            Console.WriteLine(a + b + c);
+        }
+    }
+
+    internal class CompileTime
+    {
+        static void Main(String[] args)
+        {
+
+
+            MathodOverLoading obj = new MathodOverLoading();
+            Console.WriteLine("*********Example Of Method Overloading*********");
+            obj.Show("Raju ", "More");
+            obj.Show("Rajendra ","Tatyaso ","More");
+
+        }
+    }
+}
