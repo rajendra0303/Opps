1ca182af0453c0054838355f9a519c5faceb5fc8
Rajendra More <158751784+rajendra0303@users.noreply.github.com>
2024-07-18 14:23:30 +0530

    Added Interface

---
+++ Opps/Ipiayable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps
+{
+    internal interface Ipiayable
+    {
+        void Play();
+
+    }
+    class VideoPlayer:Ipiayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing video....!");
+        }
+    }
+    class MusicPlayer:Ipiayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Music.....!");
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            VideoPlayer obj1 = new VideoPlayer();
+            MusicPlayer obj2 = new MusicPlayer();
+
+            obj1.Play();
+            obj2.Play();
+
+        }
+    }
+}
