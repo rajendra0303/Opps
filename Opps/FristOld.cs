533043af6d5bed5d2de0c55fc6170b52c107f33f
Rajendra More <158751784+rajendra0303@users.noreply.github.com>
2024-08-02 17:46:55 +0530

    Extension method added

---
+++ Opps/FristOld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectNew
+{
+    public class FirstOld
+    {
+        public int x = 10;
+        public void Add(int d)
+        {
+            Console.WriteLine("***First Base Class Calculation****");
+            Console.WriteLine($"Addition is={this.x + d}");
+        }
+
+    }
+}
---
+++ Opps/ThirdCall.cs
@@ -0,0 +1,23 @@
+﻿using DemoProjectNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps
+{
+    internal class ThirdCall
+    {
+        public static void Main()
+        { 
+        FirstOld f= new FirstOld();
+            f.Add(9);
+            f.Sub(5,4);
+            f.Mul(20);
+            f.Div(20);
+        
+        
+        }
+    }
+}
