de26a27499316f52065f02001f0f797237aa5e9b
Rajendra More <158751784+rajendra0303@users.noreply.github.com>
2024-08-02 17:44:38 +0530

    Added  ref and out keyword program

---
+++ Opps/RefKeyWord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opps
+{
+    internal class RefKeyWord
+    {
+        static void Main()
+        {
+            int incNo = 500;
+
+            Console.WriteLine("Before increament value : " + incNo);
+
+            incrementVal(ref incNo);
+
+            Console.WriteLine("After increament value : " + incNo);
+
+           
+        }
+        public static void incrementVal(ref int Number)
+        {
+            Number++;
+        }
+    }
+}
